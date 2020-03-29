aea6de722931f504b27d9f7439e6c1bf01d93ec6
Sh-Zh-7 <2431297348@qq.com>
2020-03-29 14:33:27 +0800

    Complete UI design first stage

---
+++ WallPaperCrontab/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WallPaperCrontab
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Dictionary<string, Uri> allViews = new Dictionary<string, Uri>(); //包含所有页面
+        public MainWindow()
+        {
+            InitializeComponent();
+            // 果然还要主动地绑定事件
+            MainPageLBI.AddHandler(ListBoxItem.MouseLeftButtonDownEvent, new MouseButtonEventHandler(MainPageLBI_MouseLeftButtonDown), true);
+            WallPaperLBI.AddHandler(ListBoxItem.MouseLeftButtonDownEvent, new MouseButtonEventHandler(WallPaperLBI_MouseLeftButtonDown), true);
+            QuestionsLBI.AddHandler(ListBoxItem.MouseLeftButtonDownEvent, new MouseButtonEventHandler(QuestionsLBI_MouseLeftButtonDown), true);
+            AboutUsLBI.AddHandler(ListBoxItem.MouseLeftButtonDownEvent, new MouseButtonEventHandler(AboutUsLBI_MouseLeftButtonDown), true);
+            OthersLBI.AddHandler(ListBoxItem.MouseLeftButtonDownEvent, new MouseButtonEventHandler(OthersLBI_MouseLeftButtonDown), true);
+            allViews.Add("main_page", new Uri("Pages/MainPage.xaml", UriKind.Relative));
+            allViews.Add("wall_paper", new Uri("Pages/WallPaper.xaml", UriKind.Relative));
+            allViews.Add("questions", new Uri("Pages/Questions.xaml", UriKind.Relative));
+            allViews.Add("about_us", new Uri("Pages/AboutUs.xaml", UriKind.Relative));
+            allViews.Add("others", new Uri("Pages/Others.xaml", UriKind.Relative));
+            mainFrame.Navigate(allViews["main_page"]);
+        }
+
+        private void MainPageLBI_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            mainFrame.Navigate(allViews["main_page"]);
+        }
+
+        private void WallPaperLBI_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            mainFrame.Navigate(allViews["wall_paper"]);
+        }
+
+        private void QuestionsLBI_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            mainFrame.Navigate(allViews["questions"]);
+        }
+
+        private void AboutUsLBI_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            mainFrame.Navigate(allViews["about_us"]);
+        }
+
+        private void OthersLBI_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            mainFrame.Navigate(allViews["others"]);
+        }
+    }
+}
---
+++ WallPaperCrontab/Pages/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WallPaperCrontab.Pages
+{
+    /// <summary>
+    /// Page1.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        //public class DropDownItem
+        //{
+        //    public DropDownItem(int id, string name)
+        //    {
+        //        ID = id; Name = name;
+        //    }
+        //    public int ID { get; set; }
+        //    public string Name { get; set; }
+        //}
+        public MainPage()
+        {
+            InitializeComponent();
+            //List<DropDownItem> drop_down_items = new List<DropDownItem>();
+            //drop_down_items.Add(new DropDownItem(1, "平铺"));
+            //drop_down_items.Add(new DropDownItem(2, "拉伸"));
+            //drop_down_items.Add(new DropDownItem(3, "居中"));
+            //Display.ItemsSource = drop_down_items;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                // 用分号分隔多个文件过滤的格式
+                openFileDialog.Filter = "image files (*.png, *.jpg, *.bmp, *.jpeg, *.tif, *.tiff)|*.png; *.jpg; *.bmp; *.jpeg; *.tif; *.tiff|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            // 接下来就是处理读取的图片问题了
+            System.Windows.MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButton.OK);
+        }
+    }
+
+}
