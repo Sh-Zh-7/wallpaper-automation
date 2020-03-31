73c70093c09f6551445109649bcabcfe3ffde506
Sh-Zh-7 <2431297348@qq.com>
2020-03-31 14:15:14 +0800

    Upload utils to format the project

---
+++ Utils/ImagesAttr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class ImagesAttr
+    {
+        public ImagesAttr()
+        {
+            images_path = new List<string>();
+            images_style = new List<int>();
+        }
+        public List<string> images_path { get; set; }
+        public List<int> images_style { get; set; }
+    }
+}
---
+++ Utils/TimeMode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class TimeMode
+    {
+        // 格式：MM-dd HH:mm:ss
+        public string start_time { get; set; }
+        // Second 0
+        // Minute 1
+        // Hour   2
+        // Day    3
+        public int time_mode { get; set; }
+        public double interval { get; set; }
+
+        public TimeMode(DateTime dt, int timeMode, double interval)
+        {
+            start_time = dt.ToString("MM-dd HH:mm:ss");
+            time_mode = timeMode;
+            this.interval = interval;       // C#里面没有指针
+        }
+    }
+}
