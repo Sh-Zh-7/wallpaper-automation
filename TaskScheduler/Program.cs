a923560291d05664584c8452e52c3b4beb69b7c3
Sh-Zh-7 <2431297348@qq.com>
2020-04-02 12:05:16 +0800

    Remove useless namespace

--- TaskScheduler/Program.cs
+++ TaskScheduler/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Utils;
 using System.Threading;
 
--- WallPaperChanger/Program.cs
+++ WallPaperChanger/Program.cs
@@ -1,13 +1,6 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using Utils;
 
 namespace WallPaperChanger
